64a4fc3e04bb0a57950ef4416b32f39d50af8eca
Will Knowles <will@willknowles.com>
2021-07-28 16:18:50 -0500

    Wikipedia API Setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,46 @@
-// global variable
-var appId = "faa63307397f9a8437455fc4cacc3cd2";
-// var pleaseNoCors = (event) => {
-// let  = "api.openweathermap.org/data/2.5/onecall?lat=40.43&lon=74.39" + "&appId=" + appId;
-// };
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=40.43&lon=74.39" + "&appId=" + appId)
-.then ((response) => response.json())
-.then ((data) => console.log(data))
 
+/* 
+Basic Wikipedia API Notes
+Base URL: 'http://en.wikipedia.org/w/api.php?'
+To Prevent CORS Errors: '&origin=*'
+Action:
+
+    '&action=parse': To Use Wiki Parsing API
+
+        '&page={STRING}': Gets All Properties of Page {String}
+
+            '&prop=text': Gets Text Property Of Page WikiApi
+            
+    '&action=query': To Use Wiki Query API
+            '&prop=extracts': Returns Formatted Text of Entire Wiki Page
+                
+            '&prop=extracts&exintro': Returns Formatted Text of Intro from Wiki Page
+
+            'prop=extracts&explaintext: Returns String of Plaintext from Wiki Page
+
+Format: '&format=json' 
+*/
+
+// Retrieve contents by titles and Query API
+// This test has been most successful in returning usable data from a Wikipedia Page
+// This test is retreiving an article by the Title Zebra, to get a different article we change the value of '&titles='
+
+function apiWikiTest() {
+    fetch("http://en.wikipedia.org/w/api.php?origin=*&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Zebra&format=json")
+    .then((response) => response.json())
+    .then(function(data){
+
+        //Getting the first element within the pages object
+        page = data.query.pages[Object.keys(data.query.pages)[0]]
+
+        //Setting Page Extract To HTML Element
+        $("#data").html(page.extract);
+
+        //Logging the raw data response
+        console.log("Wikipedia Query Response", data);
+
+        
+    })
+}
+
+apiWikiTest();
