0fdf522d304077ceb4e0a601dd36190e887540f3
Your Name <you@example.com>
2021-07-29 09:37:58 -0500

    local storage save items

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ fetch("https://api.openweathermap.org/data/2.5/onecall?lat=40.43&lon=74.39" + "&
 .then ((response) => response.json())
 .then ((data) => console.log(data))
 
+//Jquery starts here
+
+$(".animalType").click(function() {
+    //if mammals select then show mammals option
+    //if fish select then show fish option
+    //if insects select then show insects option
+    //if birds select then show birds option
+    //if reptiles select then show reptiles option
+});
+
+$(".animalSelection").click(function() {
+    //show information base on which actuall animal is clicked
+    //interacts with api
+    //use logic to fire up modal
+});
