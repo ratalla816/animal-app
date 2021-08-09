6a398a973b9cacb1c95f4b0c6edac162c1173934
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-08-08 22:10:18 -0500

    I tried again...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,6 @@
 /*
 Object & Persistence
 */
-$(document).ready(function(){
-    $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
-        localStorage.setItem('activeTab', $(e.target).attr('href'));
-    });
-    var activeTab = localStorage.getItem('activeTab');
-    if(activeTab){
-        $('#myTab a[href="' + activeTab + '"]').tab('show');
-    }
-});
 
 var animalDefaultObj = [
     //Mammals
